722c3ff936fab89b4ab8bc3252bd73aceecadb2c
Tosin Olawale Tomotele {N} <tomotelet@accessbankplc.com>
2024-03-04 23:56:38 +0100

    - Added claims repository and policyholder repository inside the repository manager

--- src/InsuranceSystem.RepositoryContracts/IUnitOfWork/IRepositoryManager.cs
+++ src/InsuranceSystem.RepositoryContracts/IUnitOfWork/IRepositoryManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InsuranceSystem.RepositoryContracts.IPersistenceRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace InsuranceSystem.RepositoryContracts.IUnitOfWork
 {
     public interface IRepositoryManager
     {
+        IPolicyHolderRepository PolicyHolderRepository { get; }
+        IClaimsRepository ClaimsRepository { get; }
         Task SaveAsync();
     }
 }
--- src/InsuranceSystem.RepositoryServices/UnitOfWork/RepositoryManager.cs
+++ src/InsuranceSystem.RepositoryServices/UnitOfWork/RepositoryManager.cs
@@ -1,5 +1,7 @@
 ﻿using InsuranceSystem.Entities;
+using InsuranceSystem.RepositoryContracts.IPersistenceRepository;
 using InsuranceSystem.RepositoryContracts.IUnitOfWork;
+using InsuranceSystem.RepositoryServices.PersistenceRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +13,18 @@ namespace InsuranceSystem.RepositoryServices.UnitOfWork
     public sealed class RepositoryManager : IRepositoryManager
     {
         private readonly RepositoryContext _repositoryContext;
+        private readonly Lazy<IClaimsRepository> _claimsRepository;
+        private readonly Lazy<IPolicyHolderRepository> _policyHolderRepository;
 
         public RepositoryManager(RepositoryContext repositoryContext)
         {
             _repositoryContext = repositoryContext;
+            _claimsRepository = new Lazy<IClaimsRepository>(() => new ClaimsRepository(repositoryContext));
+            _policyHolderRepository = new Lazy<IPolicyHolderRepository>(() => new PolicyHolderRepository(repositoryContext));
         }
 
+        public IClaimsRepository ClaimsRepository => _claimsRepository.Value;
+        public IPolicyHolderRepository PolicyHolderRepository => _policyHolderRepository.Value;
         public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
     }
 }
